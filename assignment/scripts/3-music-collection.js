0243649a28ad7064169082c01683fb58ab0a78b2
Solomon Lambert <sol.lambert@gmail.com>
2023-01-06 15:45:47 -0600

    finish and test search function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -29,29 +29,36 @@ function findByArtist ( artist ) {
   }
 }
 
+//The assignment is ambiguous on if it wants *ANY* or *ALL* search criteria matched
 function search ( searchAlbum = {artist, year} ) {
     let result = [];
     let artist = searchAlbum.artist;
     let year = searchAlbum.year;
-    if (searchAlbum == undefined || searchAlbum.artist == undefine || searchAlbum.year == undefined) {
+    if (searchAlbum == undefined || (searchAlbum.artist == undefined && searchAlbum.year == undefined)) {
         return collection;
     }
     for (album of collection) {
-        if (artist == album.artist && year == album.year) {
+        if (artist == album.artist || year == album.year) {
             result.push(album);
         }
     }
     return result;
 }
 
-addToCollection('In the Court of the Crimson King', 'King Crimson', '1969');
-addToCollection('No Parking on the Dance Floor', 'Midnight Star', '1983');
-addToCollection('Vagabond Dreams', 'St. Cinder', '2015');
-addToCollection('Mystery Girl', 'Roy Orbison', '1989');
-addToCollection('32Oz to Failure', 'The Antidont\'s', '2014');
-addToCollection('Desire', 'Spazzkid', '2013');
+addToCollection('In the Court of the Crimson King', 'King Crimson', 1969);
+addToCollection('No Parking on the Dance Floor', 'Midnight Star', 1983);
+addToCollection('Vagabond Dreams', 'St. Cinder', 2015);
+addToCollection('Mystery Girl', 'Roy Orbison', 1989);
+addToCollection('32Oz to Failure', 'The Antidont\'s', 2014);
+addToCollection('Desire', 'Spazzkid', 2013);
 
 showCollection();
 
 findByArtist('Spazzkid');
 findByArtist('David Bowie');
+
+console.log(search({artist: 'King Crimson', year: 1969}));
+console.log(search({artist: 'King Crimson', year: 2015}));
+console.log(search({artist: 'King Crimson'}));
+console.log(search({year: 2015}));
+console.log(search({}));
