c7aebbf1be2e5b973f6ae35689a80598d61298cc
Solomon Lambert <sol.lambert@gmail.com>
2023-01-06 14:56:41 -0600

    create collection array and addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,11 @@
 console.log('***** Music Collection *****')
+let collection = [];
+
+function addToCollection ( title, artist, yearPub ) {
+    const album {
+        title: title,
+        artist: artist,
+        yearPublished: yearPub
+    }
+    collection.push (album);
+}
